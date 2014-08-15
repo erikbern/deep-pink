c05aef86860d195cbb55339288fee90634e32850
Erik Bernhardsson <erikbern@Eriks-MacBook-Air.local>
2014-08-15 10:06:12 -0400

    new training

--- train.py
+++ train.py
@@ -10,12 +10,11 @@ import itertools
 rng = numpy.random
 
 def load_data(dir='games'):
-    X_data = []
-    y_data = []
+    X, Xp, Xr, M, Y = [], [], [], [], []
 
     def read_stuff():
         for fn in os.listdir(dir):
-            if not fn.endswith('.npy'):
+            if not fn.endswith('_2.npy'):
                 continue
 
             fn = os.path.join(dir, fn)
@@ -24,46 +23,46 @@ def load_data(dir='games'):
 
             while True:
                 try:
-                    y = numpy.load(f)
-                    x = numpy.load(f)
+                    x, xp, xr, m, y = [numpy.load(f) for i in xrange(5)]
                 except:
                     break
                 
-                yield x, y
+                yield x, xp, xr, m, y
 
-    for x, y in read_stuff():
-        y_data.append(y)
-        X_data.append(x)
+    for x, xp, xr, m, y in read_stuff():
+        X.append(x)
+        Xp.append(xp)
+        Xr.append(xr)
+        M.append(m)
+        Y.append(y)
 
-        if len(y_data) % 1000 == 0:
-            print len(y_data), '...'
+        if len(Y) % 1000 == 0:
+            print len(Y), '...'
 
-        if len(y_data) == 1000000:
+        if len(Y) == 10000:
             break
 
-    return X_data, y_data
+    return X, Xp, Xr, M, Y
 
 
 def get_data():
-    X_data, y_data = load_data()
+    X, Xp, Xr, M, Y = [numpy.array(x) for x in load_data()]
 
-    print 'read', len(X_data), 'positions'
-    
-    X_data = numpy.array(X_data)
-    y_data = numpy.array(y_data)
-
-    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.01)
+    X_train, X_test, Xp_train, Xp_test, Xr_train, Xr_test, M_train, M_test, Y_train, Y_test = train_test_split(X, Xp, Xr, M, Y, test_size=0.01)
 
     print len(X_train), 'train set', len(X_test), 'test set'
 
-    return X_train, X_test, y_train, y_test
+    return X_train, X_test, Xp_train, Xp_test, Xr_train, Xr_test, M_train, M_test, Y_train, Y_test
 
-def get_model(n_in):
-    n_hidden_layers = 4
-    n_hidden = 2048
+def get_model(n_in, Ws=[], bs=[], dropout=False, lambd=0.0):
+    n_hidden_layers = 2
+    n_hidden = 256
 
     # Declare Theano symbolic variables
     x = T.matrix("x")
+    xp = T.matrix("xp")
+    xr = T.matrix("xr")
+    m = T.vector("m")
     y = T.vector("y")
 
     def W_values(n_in, n_out):
@@ -91,29 +90,38 @@ def get_model(n_in):
         Ws.append(W)
         bs.append(b)
 
-    return (x, y, Ws, bs)
-
-def build_network(x, y, Ws, bs, dropout=False, lambd=1.0):
     # Construct Theano expression graph
     srng = theano.tensor.shared_randomstreams.RandomStreams(
         rng.randint(999999))
 
-    last_layer = x
-    n = len(Ws)
-    for l in xrange(n - 1):
-        # h = T.tanh(T.dot(last_layer, Ws[l]) + bs[l])
-        h = T.dot(last_layer, Ws[l]) + bs[l]
-        h = h * (h > 0)
+    def get_pred(input):
+        last_layer = input
+        n = len(Ws)
+        for l in xrange(n - 1):
+            # h = T.tanh(T.dot(last_layer, Ws[l]) + bs[l])
+            h = T.dot(last_layer, Ws[l]) + bs[l]
+            h = h * (h > 0)
 
-        if dropout:
-            mask = srng.binomial(n=1, p=0.5, size=h.shape)
-            last_layer = h * T.cast(mask, theano.config.floatX)
-        else:
-            last_layer = h * 0.5
+            if dropout:
+                mask = srng.binomial(n=1, p=0.5, size=h.shape)
+                last_layer = h * T.cast(mask, theano.config.floatX)
+            else:
+                last_layer = h * 0.5
 
-    p = T.tanh(T.dot(last_layer, Ws[-1]) + bs[-1])
-    xent = (y - p) ** 2
-    loss = xent.mean()
+        p = T.tanh(T.dot(last_layer, Ws[-1]) + bs[-1])
+        return p
+
+    x_p = get_pred(x)
+    xp_p = get_pred(xp)
+    xr_p = get_pred(xr)
+
+    gamma_zs, gamma_om, gamma_pp = 0.1, 10.0, 1.0
+
+    # Add loss functions
+    zs_loss = gamma_zs * abs(x_p + xp_p).mean() # zero-sum
+    om_loss = gamma_om * ((xr_p - xp_p) * (xr_p > xp_p)).mean() # optimal move
+    # pp_loss = (abs(x_p - y) / m).mean() # predict probabilities
+    pp_loss = gamma_pp * abs(x_p - y).mean() # predict probabilities
 
     # add regularization terms
     reg = 0
@@ -121,24 +129,22 @@ def build_network(x, y, Ws, bs, dropout=False, lambd=1.0):
         reg += lambd * abs(w).mean()
     for b in bs:
         reg += lambd * abs(b).mean()
-    return p, loss, reg
+
+    return x, xp, xr, m, y, Ws, bs, zs_loss, om_loss, pp_loss, reg #, x_p, xp_p, xr_p
 
 
 def train():
-    X_train, X_test, y_train, y_test = get_data()
+    X_train, X_test, Xp_train, Xp_test, Xr_train, Xr_test, M_train, M_test, Y_train, Y_test = get_data()
     n_in = len(X_train[0])
 
-    x, y, Ws, bs = get_model(n_in)
+    x, xp, xr, m, y, Ws, bs, zs_loss_f, om_loss_f, pp_loss_f, reg_f = get_model(n_in)
 
-    _, train_loss_f, train_reg_f = build_network(x, y, Ws, bs, dropout=False)
-    _, test_loss_f, _ = build_network(x, y, Ws, bs, dropout=False)
-
-    train_obj_f = train_loss_f + train_reg_f
+    obj_f = zs_loss_f + om_loss_f + pp_loss_f + reg_f
     
     momentum = 0.9
     minibatch_size = min(10000, len(X_train))
 
-    learning_rate = 0.1
+    learning_rate = 1e-1
     while True:
         print 'learning rate:', learning_rate
 
@@ -146,16 +152,19 @@ def train():
         for param in Ws + bs:
             param_update = theano.shared(numpy.zeros(param.get_value().shape))
             updates.append((param, param - learning_rate * param_update))
-            updates.append((param_update, momentum * param_update + (1. - momentum) * T.grad(train_obj_f, param)))
+            updates.append((param_update, momentum * param_update + (1. - momentum) * T.grad(obj_f, param)))
 
         # Compile
         train = theano.function(
-            inputs=[x,y],
-            outputs=[train_obj_f, train_loss_f, train_reg_f],
-            updates=updates)
+            inputs=[x, xp, xr, m, y],
+            outputs=[zs_loss_f, om_loss_f, pp_loss_f, reg_f],
+            updates=updates,
+            on_unused_input='warn')
+
         test = theano.function(
-            inputs=[x,y],
-            outputs=test_loss_f)
+            inputs=[x,xp, xr, m, y],
+            outputs=pp_loss_f,
+            on_unused_input='warn')
 
         # Train
         best_test_loss = float('inf')
@@ -164,9 +173,14 @@ def train():
         for i in itertools.count():
             minibatch_index = random.randint(0, int(len(X_train) / minibatch_size) - 1)
             lo, hi = minibatch_index * minibatch_size, (minibatch_index + 1) * minibatch_size
-            train_obj, train_loss, train_reg = train(X_train[lo:hi], y_train[lo:hi])
-            test_loss = test(X_test, y_test)
-            print '%5d: train obj: %5f (loss: %5f + reg: %5f), test loss: %5f' % (i, train_obj, train_loss, train_reg, test_loss)
+            zs_loss, om_loss, pp_loss, reg_loss = train(X_train[lo:hi], Xp_train[lo:hi], Xr_train[lo:hi], M_train[lo:hi], Y_train[lo:hi])
+            xs = [zs_loss, om_loss, pp_loss, reg_loss]
+            xs.append(sum(xs))
+
+            test_loss = test(X_test, Xp_test, Xr_test, M_test, Y_test)
+            print '\t'.join(['%12.9f' % x for x in xs]) + ' test %12.9f' % test_loss
+
+            # print '%5d: train obj: %5f (loss: %5f + reg: %5f), test loss: %5f' % (i, train_loss + train_reg, train_loss, train_reg, test_loss)
 
             if test_loss < best_test_loss:
                 best_test_loss = test_loss
