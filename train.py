14f8f3a30d9a2e9a601ce6abc8baa7638d978354
Erik Bernhardsson <erikbern@spotify.com>
2014-08-15 16:52:14 -0400

    simpler model

--- train.py
+++ train.py
@@ -31,28 +31,25 @@ def load_data(dir='games'):
 
     for x, xp, xr, m, y in read_stuff():
         X.append(x)
-        Xp.append(xp)
         Xr.append(xr)
-        M.append(m)
-        Y.append(y)
 
-        if len(Y) % 1000 == 0:
-            print len(Y), '...'
+        if len(X) % 1000 == 0:
+            print len(X), '...'
 
-        if len(Y) == 10000:
+        if len(X) == 10000:
             break
 
-    return X, Xp, Xr, M, Y
+    return X, Xr
 
 
 def get_data():
-    X, Xp, Xr, M, Y = [numpy.array(x) for x in load_data()]
+    X, Xr = [numpy.array(x) for x in load_data()]
 
-    X_train, X_test, Xp_train, Xp_test, Xr_train, Xr_test, M_train, M_test, Y_train, Y_test = train_test_split(X, Xp, Xr, M, Y, test_size=0.01)
+    X_train, X_test, Xr_train, Xr_test = train_test_split(X, Xr, test_size=0.01)
 
     print len(X_train), 'train set', len(X_test), 'test set'
 
-    return X_train, X_test, Xp_train, Xp_test, Xr_train, Xr_test, M_train, M_test, Y_train, Y_test
+    return X_train, X_test, Xr_train, Xr_test
 
 def get_model(n_in, Ws=[], bs=[], dropout=False, lambd=0.0):
     n_hidden_layers = 2
@@ -60,10 +57,7 @@ def get_model(n_in, Ws=[], bs=[], dropout=False, lambd=0.0):
 
     # Declare Theano symbolic variables
     x = T.matrix("x")
-    xp = T.matrix("xp")
     xr = T.matrix("xr")
-    m = T.vector("m")
-    y = T.vector("y")
 
     def W_values(n_in, n_out):
         return numpy.asarray(rng.uniform(
@@ -108,20 +102,14 @@ def get_model(n_in, Ws=[], bs=[], dropout=False, lambd=0.0):
             else:
                 last_layer = h * 0.5
 
-        p = T.tanh(T.dot(last_layer, Ws[-1]) + bs[-1])
+        # p = T.tanh(T.dot(last_layer, Ws[-1]) + bs[-1])
+        p = T.dot(last_layer, Ws[-1]) + bs[-1]
         return p
 
     x_p = get_pred(x)
-    xp_p = get_pred(xp)
     xr_p = get_pred(xr)
 
-    gamma_zs, gamma_om, gamma_pp = 0.1, 10.0, 1.0
-
-    # Add loss functions
-    zs_loss = gamma_zs * abs(x_p + xp_p).mean() # zero-sum
-    om_loss = gamma_om * ((xr_p - xp_p) * (xr_p > xp_p)).mean() # optimal move
-    # pp_loss = (abs(x_p - y) / m).mean() # predict probabilities
-    pp_loss = gamma_pp * abs(x_p - y).mean() # predict probabilities
+    loss = T.log(T.sigm(x_p - xr_p))
 
     # add regularization terms
     reg = 0
@@ -130,16 +118,16 @@ def get_model(n_in, Ws=[], bs=[], dropout=False, lambd=0.0):
     for b in bs:
         reg += lambd * abs(b).mean()
 
-    return x, xp, xr, m, y, Ws, bs, zs_loss, om_loss, pp_loss, reg #, x_p, xp_p, xr_p
+    return x, xr, Ws, bs, loss, reg #, x_p, xp_p, xr_p
 
 
 def train():
     X_train, X_test, Xp_train, Xp_test, Xr_train, Xr_test, M_train, M_test, Y_train, Y_test = get_data()
     n_in = len(X_train[0])
 
-    x, xp, xr, m, y, Ws, bs, zs_loss_f, om_loss_f, pp_loss_f, reg_f = get_model(n_in)
+    x, xr, Ws, bs, loss_f, reg_f = get_model(n_in)
 
-    obj_f = zs_loss_f + om_loss_f + pp_loss_f + reg_f
+    obj_f = loss_f + reg_f
     
     momentum = 0.9
     minibatch_size = min(10000, len(X_train))
@@ -156,14 +144,14 @@ def train():
 
         # Compile
         train = theano.function(
-            inputs=[x, xp, xr, m, y],
-            outputs=[zs_loss_f, om_loss_f, pp_loss_f, reg_f],
+            inputs=[x, xr],
+            outputs=[loss_f, reg_f],
             updates=updates,
             on_unused_input='warn')
 
         test = theano.function(
-            inputs=[x,xp, xr, m, y],
-            outputs=pp_loss_f,
+            inputs=[x, xr],
+            outputs=loss_f,
             on_unused_input='warn')
 
         # Train
@@ -173,11 +161,11 @@ def train():
         for i in itertools.count():
             minibatch_index = random.randint(0, int(len(X_train) / minibatch_size) - 1)
             lo, hi = minibatch_index * minibatch_size, (minibatch_index + 1) * minibatch_size
-            zs_loss, om_loss, pp_loss, reg_loss = train(X_train[lo:hi], Xp_train[lo:hi], Xr_train[lo:hi], M_train[lo:hi], Y_train[lo:hi])
-            xs = [zs_loss, om_loss, pp_loss, reg_loss]
+            loss, reg = train(X_train[lo:hi], Xr_train[lo:hi])
+            xs = [loss, reg]
             xs.append(sum(xs))
 
-            test_loss = test(X_test, Xp_test, Xr_test, M_test, Y_test)
+            test_loss = test(X_test, Xr_test)
             print '\t'.join(['%12.9f' % x for x in xs]) + ' test %12.9f' % test_loss
 
             # print '%5d: train obj: %5f (loss: %5f + reg: %5f), test loss: %5f' % (i, train_loss + train_reg, train_loss, train_reg, test_loss)
