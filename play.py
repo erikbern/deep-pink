ed21f99e772b21a2b8e54938b7ea6e1b611d9ec3
Erik Bernhardsson <erikbern@spotify.com>
2014-11-20 19:03:37 -0500

    attempt at using sunfish

--- play.py
+++ play.py
@@ -7,6 +7,11 @@ import chess, chess.pgn
 from parse_game import bb2array
 import heapq
 import time
+import re
+import string
+import numpy
+import sunfish
+
 
 def get_model_from_pickle(fn):
     f = open(fn)
@@ -21,50 +26,51 @@ def get_model_from_pickle(fn):
 
     return predict
 
+strip_whitespace = re.compile(r"\s+")
+translate_pieces = string.maketrans(".PNBRQKpnbrqk", "\x00" + "\x01\x02\x03\x04\x05\x06" + "\x08\x09\x0a\x0b\x0c\x0d")
 
-def negamax(gn_current, depth, alpha, beta, color, func):
-    b = gn_current.board()
-    # print depth, alpha, beta, color
-    # print b
+def sf2array(pos):
+    # Create a numpy array from a sunfish representation
+    pos = strip_whitespace.sub('', pos.board) # should be 64 characters now
+    pos = pos.translate(translate_pieces)
+    return numpy.fromstring(pos, dtype=numpy.int8)
 
-    if b.is_checkmate():
+
+def negamax(pos, depth, alpha, beta, color, func):
+    if pos.board.find('K') == -1:
         return float('-inf'), None
-    elif b.is_stalemate():
-        return 0.0, None
+    # Should do stalemate stuff too, not sure how
 
-    gn_candidates = []
+    moves = []
     X = []
-    for move in gn_current.board().legal_moves:
-        gn_candidate = chess.pgn.GameNode()
-        gn_candidate.parent = gn_current
-        gn_candidate.move = move
-        gn_candidates.append(gn_candidate)
-        b = gn_candidate.board()
-        flip = bool(b.turn == 0)
-        X.append(bb2array(b, flip=flip))
+    for move in pos.genMoves():
+        pos_child = pos.move(move)
+        moves.append(move)
+        X.append(sf2array(pos_child))
 
     if len(X) == 0:
         raise Exception('eh?')
         # TODO: should treat checkmate
 
     # Use model to predict scores
-    scores = func(X)
+    scores = -func(X)
 
-    child_nodes = sorted(zip(scores, gn_candidates), reverse=(color==1))
+    child_nodes = sorted(zip(scores, moves), reverse=(color==1))
 
     best_value = float('-inf')
     best_move = None
     
-    for score, gn_candidate in child_nodes:
+    for score, move in child_nodes:
         if depth == 1:
             value = score # * color
         else:
-            neg_value, _ = negamax(gn_candidate, depth-1, -beta, -alpha, -color, func)
+            pos_child = pos.move(move)
+            neg_value, _ = negamax(pos_child, depth-1, -beta, -alpha, -color, func)
             value = -neg_value
 
         if value > best_value:
             best_value = value
-            best_move = gn_candidate.move
+            best_move = move
 
         if value > alpha:
             alpha = value
@@ -83,22 +89,37 @@ class Player(object):
 class Computer(Player):
     def __init__(self, func):
         self._func = func
+        self._pos = sunfish.Position(sunfish.initial, 0, (True,True), (True,True), 0, 0)
 
     def move(self, gn_current):
         assert(gn_current.board().turn == 0)
+
+        if gn_current.move is not None:
+            print 'applying last move'
+            # Apply last_move
+            crdn = str(gn_current.move)
+            move = (119 - sunfish.parse(crdn[0:2]), 119 - sunfish.parse(crdn[2:4]))
+            print gn_current.move, move
+            self._pos = self._pos.move(move)
 
         for depth in xrange(1, 5):
             alpha = float('-inf')
             beta = float('inf')
             
             t0 = time.time()
-            best_value, best_move = negamax(gn_current, depth, alpha, beta, 1, self._func)
+            best_value, best_move = negamax(self._pos, depth, alpha, beta, 1, self._func)
             print depth, best_value, best_move, time.time() - t0
             depth += 1
 
+
+        self._pos = self._pos.move(best_move)
+        crdn = sunfish.render(best_move[0]) + sunfish.render(best_move[1])
+        move = chess.Move.from_uci(crdn)
+        
         gn_new = chess.pgn.GameNode()
         gn_new.parent = gn_current
-        gn_new.move = best_move
+        gn_new.move = move
+
 
         return gn_new
 
@@ -136,7 +157,6 @@ class Human(Player):
 
 class Sunfish(Player):
     def __init__(self):
-        import sunfish
         self._pos = sunfish.Position(sunfish.initial, 0, (True,True), (True,True), 0, 0)
 
     def move(self, gn_current):
@@ -146,8 +166,9 @@ class Sunfish(Player):
 
         # Apply last_move
         crdn = str(gn_current.move)
+        print 'last move:', crdn
         move = (sunfish.parse(crdn[0:2]), sunfish.parse(crdn[2:4]))
-        self._pos = self._pos.move(move)            
+        self._pos = self._pos.move(move)
 
         move, score = sunfish.search(self._pos)
         self._pos = self._pos.move(move)
